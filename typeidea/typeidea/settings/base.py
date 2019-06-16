7751317331b162637982246bc1e7abcc3e74f89e
xchuan <1597007399@qq.com>
2019-06-16 17:24:53 +0800

    修改settings 修改manage.py

--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -70,15 +70,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'typeidea.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
